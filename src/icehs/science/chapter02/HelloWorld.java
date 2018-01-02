856ec72996f2fb5a4d5939696d8301b3cbde621e
admin <admin@10.137.138.164>
2018-01-02 16:51:14 +0900

    chapter02

---
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -0,0 +1,12 @@
+package icehs.science.chapter02;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+		System.out.println("환영합니다. 여러분");
+		System.out.println("sysout 입력 후 crtl + space를 누르시오.");
+		System.out.println("쌍따음표 사이에 글을 입력한 후 F11을 누르시오.");
+	}
+
+}
