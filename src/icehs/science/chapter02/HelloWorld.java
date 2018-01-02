7339e871257f6e5bd75510668edd3ed04e549825
admin <admin@10.137.138.164>
2018-01-02 17:06:23 +0900

    chapter02

--- src/icehs/science/chapter02/HelloWorld.java
+++ src/icehs/science/chapter02/HelloWorld.java
@@ -1,12 +1,18 @@
 package icehs.science.chapter02;
 
 public class HelloWorld {
-
+	//나의 첫번째 Java 프로그램
+	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
-		System.out.println("환영합니다. 여러분");
-		System.out.println("sysout 입력 후 crtl + space를 누르시오.");
-		System.out.println("쌍따음표 사이에 글을 입력한 후 F11을 누르시오.");
+		//메인 메서드 안에 주석 달기
+		/*
+ 			여러 줄을
+ 			주석 처리할 떄
+		*/
+			System.out.println("Hello World!");
+			System.out.println("환영합니다. 여러분");
+			System.out.println("sysout 입력 후 crtl + space를 누르시오.");
+			System.out.println("쌍따음표 사이에 글을 입력한 후 F11을 누르시오.");
 	}
 
 }
